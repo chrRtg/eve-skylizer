5c73cabfac497fc4a7dd7b4314d45a8a46a8579b
Rtg Quack <chr@quack>
2018-05-25 21:46:38 +0200

    Add structures and player given names to a moon.

--- public/js/sl.js
+++ public/js/sl.js
@@ -1,6 +1,51 @@
 $(document).ready(function ()
 {
+	//datatable for moonTable
+	$('#moontable').DataTable();
+	//$(document).ajaxStart($.blockUI).ajaxStop($.unblockUI);
+
+
+	$('#structureEditModal').on('show.bs.modal', function (e) {
+		var called_by = e.relatedTarget; // calling object (the link to open the modal)
+		$("#structureEditFormMoonId").val( called_by.getAttribute('data-moonid') ); // insert moonIt into structure edit form modal
+		$("#structureEditFormStructureId").val( called_by.getAttribute('data-structid') ); // insert moonIt into structure edit form modal
+		$("#structeditname").val( called_by.getAttribute('data-structgivename') ); // insert player given name
+		var structtype = called_by.getAttribute('data-structtype');
+		$("#structedittype").val( (structtype ? structtype : 0 ) ); // set proper selection
+	})
 	
+	// activate structure edit form submit button
+	$("#structureEditFormSubmit").on('click', function () {
+		
+		$.ajax({
+			url: $(this.form).attr('action'),
+			type: "post",
+			data: $(this.form).serializeArray(),
+			beforeSend: function (e) { $("#structureEditModal").block({ message: '<h1>update structure...</h1>' }); }, 
+			complete: function (e) { 
+				location.reload(); // no complex auto update page yet
+				//$('#structureEditModal').modal('hide');
+				//$("#structureEditModal").unblock(); 
+			}, 
+			success: function (data, status) {
+				//
+			},
+			error: function(xhr) {
+				$.toast({
+					text: "An AJAX error occured: " + xhr.status + " " + xhr.statusText,
+					heading: 'ERROR',
+					icon: 'error',
+					hideAfter: 5000,
+					position: 'top-right',
+					loader: true,
+					loaderBg: '#4e433c'
+				});				
+			}
+		});
+	});
+
+
+
 	// auto-submit checkboxes
 	$("#detail_filter_composition").change(function() {
 		if (this.checked) {
@@ -30,7 +75,7 @@ $(document).ready(function ()
 	});
 	
 	
-	// activate select2 for some elements
+	// activate select2 for filters
 	$('#selectcomposition').select2();
 	$('#selectore').select2();
 
@@ -42,6 +87,7 @@ $(document).ready(function ()
 		var data = e.params.data;
 		buildFilterQuery({type:"selectore", id:e.params.data.id});
 	});
+
 
 	/**
 	 * Select2 typeahead to select a system or constellation
@@ -78,134 +124,6 @@ $(document).ready(function ()
 		templateResult: formatRepo,
 		templateSelection: formatRepoSelection
 	});
-
-	/**
-	 * Format select2 drop down elements
-	 * 
-	 * @param {array} repo
-	 * @return {String}
-	 */
-	function formatRepo(repo) {
-
-		if (repo.loading) {
-			return repo.text;
-		}
-
-		var markup = "<div class='select2-result-repository clearfix'>" +
-				"<div class='select2-result-repository__title'>" + repo.itemname + "</div>";
-		if(repo.constellation == null) {
-			markup += "<div class='select2-result-repository__description'>Constellation in " + repo.region + "</div>";
-		} else {
-			markup += "<div class='select2-result-repository__description'>" + repo.constellation + " / " + repo.region + "</div>";
-		}
-		
-		
-		markup += "</div>";
-
-		return markup;
-	}
-
-	/**
-	 * Format select2 selected value
-	 * 
-	 * @param {array} repo
-	 * @return {String}
-	 */
-	function formatRepoSelection(repo) {
-		if(repo.itemname == null) {
-			return repo.text;
-		}
-		
-		if(repo.constellation == null) {
-			return repo.itemname + ' (Constellation in '+repo.region+')';
-		} else {
-			return repo.itemname + '  ('+repo.region+')';
-		}
-	}
-
-	function setLocationFilter(loc_id) {
-		buildFilterQuery({type:"selectsystem", id:loc_id});
-	}
-
-	/**
-	 * Takes the filter params as well a request. 
-	 * 
-	 * Out of all previous filters as well the current request this function builds a 
-	 * filtered query and send him to the server via location.href
-	 * 
-	 * 
-	 * @param {type} filter_param
-	 * @return {Boolean}
-	 */
-	function buildFilterQuery(filter_param) 
-	{
-		var jdata = null;
-		var url_param = {};
-		var url = '';
-		
-		$.blockUI({ message: "<h1>update the page...</h1>",
-			css: { 
-				border: 'none', 
-				padding: '15px', 
-				backgroundColor: '#000', 
-				'-webkit-border-radius': '10px', 
-				'-moz-border-radius': '10px', 
-				opacity: .5, 
-				color: '#fff' 
-			} }); 
-		
-		if (typeof filters_json === 'undefined' || !filters_json) {
-			console.debug('no filter param given');
-		}
-		
-		if (typeof filters_json !== 'undefined' && filters_json) {
-			jdata = JSON.parse(filters_json);
-			url_param['system'] = (jdata['system'] ? jdata['system'] : null);
-			url_param['ore'] = (jdata['ore'] ? jdata['ore'] : null);
-			url_param['composition'] = (jdata['composition'] ? jdata['composition'] : null);
-		}
-		
-		if(filter_param) {
-			if(filter_param.type === 'selectsystem') {
-				url_param['system'] = filter_param.id;
-			}
-			if(filter_param.type === 'selectcomposition') {
-				url_param['composition'] = filter_param.id;
-			}
-			if(filter_param.type === 'selectore') {
-				url_param['ore'] = filter_param.id;
-			}
-			if(filter_param.type === 'detail_filter_composition') {
-				url_param['detail_filter_composition'] = filter_param.id;
-			}
-			if(filter_param.type === 'detail_filter_ore') {
-				url_param['detail_filter_ore'] = filter_param.id;
-			}
-		}
-		
-		if(url_param) {
-			for (var key in url_param) {
-//				if(url_param[key] && url_param[key] != 'null') {
-				if(url_param[key] != null && url_param[key] != 'null') {
-					url += (url ? '&' : '') + key + '=' + url_param[key];
-				}
-			}
-			console.log(url);
-			if(url && url != '') {
-				console.log('gogo: ' +  url);
-				location.href = '/vposmoon?' + url;
-				return false;
-			}
-			console.log('oerks');
-		}
-		console.log('MEGA oerks');
-		//location.href = '/vposmoon';
-		return false;
-	}
-
-
-	//datatable for moonTable
-	$('#moontable').DataTable();
 
 	// show toaster messages
 	// see http://kamranahmed.info/toast
@@ -259,5 +177,122 @@ $(document).ready(function ()
 			}
 		}
 	}
-
 });
+
+
+
+
+/**
+ * Format select2 drop down elements
+ * 
+ * @param {array} repo
+ * @return {String}
+ */
+function formatRepo(repo) {
+
+	if (repo.loading) {
+		return repo.text;
+	}
+
+	var markup = "<div class='select2-result-repository clearfix'>" +
+			"<div class='select2-result-repository__title'>" + repo.itemname + "</div>";
+	if(repo.constellation == null) {
+		markup += "<div class='select2-result-repository__description'>Constellation in " + repo.region + "</div>";
+	} else {
+		markup += "<div class='select2-result-repository__description'>" + repo.constellation + " / " + repo.region + "</div>";
+	}
+
+
+	markup += "</div>";
+
+	return markup;
+}
+
+/**
+ * Format select2 selected value
+ * 
+ * @param {array} repo
+ * @return {String}
+ */
+function formatRepoSelection(repo) {
+	if(repo.itemname == null) {
+		return repo.text;
+	}
+
+	if(repo.constellation == null) {
+		return repo.itemname + ' (Constellation in '+repo.region+')';
+	} else {
+		return repo.itemname + '  ('+repo.region+')';
+	}
+}
+
+function setLocationFilter(loc_id) {
+	buildFilterQuery({type:"selectsystem", id:loc_id});
+}
+
+/**
+ * Takes the filter params as well a request. 
+ * 
+ * Out of all previous filters as well the current request this function builds a 
+ * filtered query and send him to the server via location.href
+ * 
+ * 
+ * @param {type} filter_param
+ * @return {Boolean}
+ */
+function buildFilterQuery(filter_param) 
+{
+	var jdata = null;
+	var url_param = {};
+	var url = '';
+
+	$.blockUI({ message: "<h1 class=\"block-overlay\">update the page...</h1>"}); 
+
+	if (typeof filters_json === 'undefined' || !filters_json) {
+		console.debug('no filter param given');
+	}
+
+	if (typeof filters_json !== 'undefined' && filters_json) {
+		jdata = JSON.parse(filters_json);
+		url_param['system'] = (jdata['system'] ? jdata['system'] : null);
+		url_param['ore'] = (jdata['ore'] ? jdata['ore'] : null);
+		url_param['composition'] = (jdata['composition'] ? jdata['composition'] : null);
+	}
+
+	if(filter_param) {
+		if(filter_param.type === 'selectsystem') {
+			url_param['system'] = filter_param.id;
+		}
+		if(filter_param.type === 'selectcomposition') {
+			url_param['composition'] = filter_param.id;
+		}
+		if(filter_param.type === 'selectore') {
+			url_param['ore'] = filter_param.id;
+		}
+		if(filter_param.type === 'detail_filter_composition') {
+			url_param['detail_filter_composition'] = filter_param.id;
+		}
+		if(filter_param.type === 'detail_filter_ore') {
+			url_param['detail_filter_ore'] = filter_param.id;
+		}
+	}
+
+	if(url_param) {
+		for (var key in url_param) {
+//				if(url_param[key] && url_param[key] != 'null') {
+			if(url_param[key] != null && url_param[key] != 'null') {
+				url += (url ? '&' : '') + key + '=' + url_param[key];
+			}
+		}
+		console.log(url);
+		if(url && url != '') {
+			console.debug('gogo: ' +  url);
+			location.href = '/vposmoon?' + url;
+			return false;
+		}
+		//console.log('oerks');
+	}
+	// console.log('MEGA oerks');
+	//location.href = '/vposmoon';
+	return false;
+}
