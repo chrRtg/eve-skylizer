2b955e601fc34e4e0c18e4f512aacefff563f090
Rtg Quack <chr@quack>
2018-06-03 21:37:44 +0200

    Fetch Alliances and Corporation Data from ESI and persist to local DB

--- module/Application/src/Service/EveDataManager.php
+++ module/Application/src/Service/EveDataManager.php
@@ -119,7 +119,6 @@ class EveDataManager {
 		return($queryBuilder->getQuery()->getResult(\Doctrine\ORM\Query::HYDRATE_SCALAR));
 	}
 
-
 	/**
 	 * Fetch a list of Eve Items by their groupID.
 	 * 
@@ -129,10 +128,10 @@ class EveDataManager {
 	 */
 	public function getTypeByGroupIDs($ids)
 	{
-		if (!is_array($ids) ) {
+		if (!is_array($ids)) {
 			throw new Exception('Parameter for getTypeByGroupIDs() must be an array!');
 		}
-		
+
 		$queryBuilder = $this->entityManager->createQueryBuilder();
 
 		$queryBuilder->select('it.typeid, it.typename')
@@ -143,7 +142,6 @@ class EveDataManager {
 
 		return($queryBuilder->getQuery()->getResult(\Doctrine\ORM\Query::HYDRATE_SCALAR));
 	}
-
 
 	/**
 	 * Fetch inventory prices from EVE-ESI. 
@@ -163,14 +161,12 @@ class EveDataManager {
 		$this->logger->debug('updatePrices :: prices cnt: ' . count($prices));
 		// better than get_object_vars() cause working in all cases;
 		$prices_data = (array) $prices;
- 		// $this->logger->debug('prices_data cnt: ' . count($prices_data));
-
+		// $this->logger->debug('prices_data cnt: ' . count($prices_data));
 		// transform prices into a associative array with the typeID as key
 		foreach ($prices_data as $price) {
 			$price_arr[$price->type_id] = (!empty($price->average_price) ? $price->average_price : (!empty($price->adjusted_price) ? $price->adjusted_price : 0.0));
 		}
 		// $this->logger->debug('price_arr cnt: ' . count($price_arr));
-
 		// no prices, no need to persist
 		if (!count($price_arr)) {
 			return 0;
@@ -206,4 +202,254 @@ class EveDataManager {
 
 		return($j);
 	}
+
+
+	/**
+	 * Fetch Alliance and Corporation Information from ESI.
+	 * 
+	 * To be called from the shell
+	 * 
+	 * @param bool $force
+	 * @param char $mode	a for alliance, c for corporation or b for both
+	 * @return string	Information about the actions taken
+	 */
+	public function updateAllyCorp($force = false, $mode = false)
+	{
+		$ally_cnt = 0;
+		$corp_cnt = 0;
+
+		if($force) {
+			echo PHP_EOL . 'Force mode enabled, fetch all' . PHP_EOL;
+		}
+
+		if($mode && ($mode == 'a' || $mode == 'b')) {
+			echo PHP_EOL . 'Fetch Alliances' . PHP_EOL;
+			$ally_cnt = $this->updateAllianceESI($force);
+		}
+
+		if($mode && ($mode == 'c' || $mode == 'b')) {
+			echo PHP_EOL . 'Fetch corporations' . PHP_EOL;
+			$corp_cnt = $this->updateCorporationESI($force);
+		}
+
+		return ($ally_cnt . ' alliances and ' . $corp_cnt . ' Corporations were updated');
+	}
+
+	/**
+	 * Fetch all alliances and their name and ticker from ESI.
+	 * 
+	 * Name and ticker are writen if the allianceID is not found in the local database.
+	 * 
+	 * Sequence of calls:
+	 * 
+	 *  # List all active player alliances
+	 *  https://esi.evetech.net/latest/alliances/?datasource=tranquility
+	 *
+	 * # Public information about an alliance
+	 * https://esi.evetech.net/latest/alliances/1354830081/?datasource=tranquility
+	 *
+	 * 
+	 * @param type $force_update Force an update regardless if the allianceID is present in the database
+	 * @return int
+	 */
+	private function updateAllianceESI($force_update = false)
+	{
+
+		// get a list of all alliances from ESI
+		$ally_list = $this->eveESIManager->publicRequest('get', '/alliances', []);
+
+		$ally_list_arr = (array) $ally_list;
+		if (!count($ally_list_arr)) {
+			return 0;
+		}
+
+		//$this->logger->debug('ally_list: ' . print_r($ally_list,true));
+		//$this->logger->debug('ally_list_arr: ' . print_r($ally_list_arr,true));
+
+		$batch_size = 50;
+		$i = 1;
+		foreach ($ally_list_arr as $row) {
+			$alliance_entity = $this->entityManager->getRepository(EveAlly::class)->findOneByAllianceId((int) $row);
+
+			if (!$alliance_entity) {
+				$alliance_entity = new EveAlly;
+			} else if ($alliance_entity->getAllianceName() && !$force_update) {
+				// if alliance has a name and it's not Force-Mode skip this alliance (presuming it's already up to date)
+				echo('S');
+				continue;
+			}
+
+
+			// fetch alliance details from ESI
+			$ally_detail = $this->eveESIManager->publicRequest('get', '/alliances/{alliance_id}/', ['alliance_id' => $row]);
+			$this->logger->debug('ally_detail: ' . print_r($ally_detail, true));
+
+			if (isset($ally_detail->name)) {
+				$alliance_entity->setAllianceName($ally_detail->name);
+			}
+			if (isset($ally_detail->ticker)) {
+				$alliance_entity->setTicker($ally_detail->ticker);
+			}
+
+			$alliance_entity->setAllianceId($row);
+			$this->entityManager->persist($alliance_entity);
+
+			if (($i % $batch_size) === 0) {
+				$this->entityManager->flush();
+				$this->entityManager->clear(); // Detaches all objects from Doctrine!
+				echo '-W' . PHP_EOL;
+			}
+
+			if (($i % 5) === 0) {
+				if (($i % 10) === 0) {
+					echo $i;
+				} else {
+					echo ':';
+				}
+			} else {
+				echo '.';
+			}
+			++$i;
+		}
+
+		$this->entityManager->flush(); //Persist objects that did not make up an entire batch
+		$this->entityManager->clear();
+		echo '#W' . PHP_EOL;
+
+		return($i);
+	}
+
+	/**
+	 * Fetch all Corporations and their name and ticker from ESI.
+	 * 
+	 * Name and ticker are writen if the corporationID is not found in the local database or
+	 * if the alliance assignment has been changed.
+	 * 
+	 * Sequence of calls:
+	 * 
+	 * # List all current member corporations of an alliance
+	 * https://esi.evetech.net/latest/alliances/1354830081/corporations/?datasource=tranquility
+	 *
+	 * # Public information about a corporation
+	 * https://esi.evetech.net/latest/corporations/98002591/?datasource=tranquility
+	 * 
+	 * 
+	 * @param type $force_update Force an update regardless if the allianceID is present in the database
+	 * @return int
+	 */
+	private function updateCorporationESI($force_update = false)
+	{
+		$batch_size = 50;
+		$i = 1;
+
+		if ($force_update) {
+			$q = $this->entityManager->createQuery('SELECT a FROM Application\Entity\EveAlly a');
+		} else {
+			// fetch only empty alliances (without corporations)
+			$queryBuilder = $this->entityManager->createQueryBuilder();
+			$q = $queryBuilder->select('a')
+				->from(EveAlly::class, 'a')
+				->leftJoin(EveCorporation::class, 'c', 'WITH', 'c.allianceId = a.allianceId')
+				->where('c.corporationId IS NULL')
+				//->setMaxResults(8)
+				->getQuery();
+		}
+
+		$iterableAllyResult = $q->iterate();
+		// iterate through all alliances in DB
+		foreach ($iterableAllyResult as $row) {
+			// do stuff with the data in the row, $row[0] is always the object
+			$alliance_id = $row[0]->getAllianceId();
+
+			// get a list of all alliances from ESI
+			$ally_members_list = $this->eveESIManager->publicRequest('get', '/alliances/{alliance_id}/corporations/', ['alliance_id' => $alliance_id]);
+
+			$ally_members_arr = (array) $ally_members_list;
+			if (!count($ally_members_arr)) {
+				continue;
+			}
+
+			echo $row[0]->getAllianceName() . ' ';
+
+			$i = 1;
+			// iterate through all corporation ESI give us for the current alliance
+			foreach ($ally_members_arr as $corporation_id) {
+				$this->updateCorporation($corporation_id);
+
+				if (($i % $batch_size) === 0) {
+					$this->entityManager->flush();
+					$this->entityManager->clear(); // Detaches all objects from Doctrine!
+					echo '-W';
+				}
+
+				if (($i % 5) === 0) {
+					if (($i % 10) === 0) {
+						echo $i;
+					} else {
+						echo ':';
+					}
+				} else {
+					echo '.';
+				}
+				++$i;
+			}
+			$this->entityManager->flush(); //Persist objects that did not make up an entire batch
+			$this->entityManager->clear();
+			echo 'W#';
+
+			// detach from Doctrine, so that it can be Garbage-Collected immediately
+			$this->entityManager->detach($row[0]);
+
+			echo ' (' . $i . ')' . PHP_EOL;
+		}
+
+		return($i);
+	}
+
+	/**
+	 * Fetch corporation details from ESI and insert or update them into the database.
+	 * 
+	 * Attention: This method is build for batch processing. To finally store the data to
+	 * the database you have to coll the EntityManager with flush().
+	 * 
+	 * @param int $corporation_id
+	 * @param int $alliance_id
+	 * @param bool $force_update
+	 * @return bool
+	 */
+	private function updateCorporation($corporation_id)
+	{
+		$corporation_detail = $this->eveESIManager->publicRequest('get', '/corporations/{corporation_id}/', ['corporation_id' => $corporation_id]);
+
+		// fetch corporation
+		$corporation_entity = $this->entityManager->getRepository(EveCorporation::class)->findOneByCorporationId((int) $corporation_id);
+
+		if (!$corporation_entity) {
+			$corporation_entity = new EveCorporation();
+			$corporation_entity->setCorporationId($corporation_id);
+		}
+
+		if (isset($corporation_detail->name)) {
+			$corporation_entity->setCorporationName($corporation_detail->name);
+		}
+
+		if (isset($corporation_detail->ticker)) {
+			$corporation_entity->setTicker($corporation_detail->ticker);
+		}
+
+		if (isset($corporation_detail->alliance_id)) {
+			$corporation_entity->setAllianceId($corporation_detail->alliance_id);
+		}
+
+
+		$this->entityManager->persist($corporation_entity);
+
+		return(true);
+	}
+
+	public function ping($param = '')
+	{
+		return('EveDataManager-ping ' . $param);
+	}
+
 }
